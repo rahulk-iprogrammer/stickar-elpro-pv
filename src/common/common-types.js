fe13c3c170a65dedcff97f94a6dd3b610fffafaf
Himanshi Sahu <himanshis@iprogrammer.co>
2026-09-16 11:27:02 +0530

    stage url change into uat url

--- src/common/common-types.js
+++ src/common/common-types.js
@@ -5,7 +5,7 @@ export const WHATSAPP_SHARING_LINK = "https://api.whatsapp.com/send";
 const { REACT_APP_BASE_URL, NODE_ENV } = process.env;
 // console.warn(REACT_APP_BASE_URL);
 console.warn(NODE_ENV);
-export const apiData = {baseUrl: NODE_ENV === "development" ? "https://stage-api.technovatechnologys.com" : REACT_APP_BASE_URL,
+export const apiData = {baseUrl: NODE_ENV === "development" ? "https://uat-api.technovatechnologys.com" : REACT_APP_BASE_URL,
   merchant_id: 10073, //10076
   platform: 4,
   appVersion: 4.0,
